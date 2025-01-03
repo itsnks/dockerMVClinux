840cfa8ba6fc61cc9e5ca9ededcdcb8fe1fa5a2d
Nikesh <subedi.nikesh@outlook.com>
2025-01-03 15:13:25 +0545

    Add project files.

---
+++ dockerMVClinux/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dockerMVClinux.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
